53615cf1870da765343a5d396851909df98961eb
Raul Miranda <raulomiranda@hotmail.com>
2023-10-07 18:39:26 -0300

    Mudar informação botão e imagem

--- js/app.js
+++ js/app.js
@@ -1,24 +1,17 @@
-// Zera todos os botões por padrão em alugar
-function zerarBotoes(){
-let botoes = document.querySelectorAll("a");
-botoes.forEach(function(zerar){
-    zerar.innerHTML = "Alugar";
-    zerar.setAttribute("class", "dashboard__item__button");
-})
-}
-//Zera todas as imagenss por padrão em alugar 
-function zerarImagens(){
-let botoes = document.querySelectorAll("div");
-botoes.forEach(function(zerar){
-    zerar.setAttribute("class", "dashboard__item__img");
-})
-}
-
-zerarImagens()
-zerarBotoes()
-
-
 // Reconhece o click do botão determinado
 function alterarStatus(id){
-    console.log(`Botão ${id} acionado`);
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector(".dashboard__item__img");
+    let botao = gameClicado.querySelector(".dashboard__item__button");
+    let nomeJogo = gameClicado.querySelector(".dashboard__item__name");
+    
+    if(botao.innerHTML == "Alugar"){
+        botao.setAttribute("class", "dashboard__item__button dashboard__item__button--return");
+        imagem.setAttribute("class", "dashboard__item__img dashboard__item__img--rented")
+        botao.innerHTML = "Devolver"
+    }else{
+        botao.setAttribute("class", "dashboard__item__button");
+        imagem.setAttribute("class", "dashboard__item__img");
+        botao.innerHTML = "Alugar"
+    }
 }
