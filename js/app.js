e6e21df1d5978619cd7fb07e142808cc3b0196a1
Raul Miranda <raulomiranda@hotmail.com>
2023-10-07 20:03:18 -0300

    Aplicar boas práticas ao código

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector(".dashboard__item__img");
     let botao = gameClicado.querySelector(".dashboard__item__button");
-    let nomeJogo = gameClicado.querySelector(".dashboard__item__name");
+    //let nomeJogo = gameClicado.querySelector(".dashboard__item__name");
     
     if(botao.innerHTML == "Alugar"){
-        botao.setAttribute("class", "dashboard__item__button dashboard__item__button--return");
-        imagem.setAttribute("class", "dashboard__item__img dashboard__item__img--rented")
+        botao.classList.add(" dashboard__item__button--return");
+        imagem.classList.add("dashboard__item__img--rented")
         botao.innerHTML = "Devolver"
     }else{
-        botao.setAttribute("class", "dashboard__item__button");
-        imagem.setAttribute("class", "dashboard__item__img");
+        botao.classList.remove("dashboard__item__button--return");
+        imagem.classList.remove("dashboard__item__img--rented");
         botao.innerHTML = "Alugar"
     }
 }
